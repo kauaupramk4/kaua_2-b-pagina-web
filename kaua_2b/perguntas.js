23d7af5d93b0426fe2d2c68ac02c44767344c9fa
cdl wuinchester <kaua.almeida25@escola.pr.gov.br>
2025-08-08 19:21:34 -0300

    Add files via upload

---
+++ kaua_2b/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Mecanica auto,motiva',
+    'O que é mecanica',
+    'Mecânica automotiva é o ramo que estuda, projeta, desenvolve, fabrica e repara veículos automotores, abrangendo desde motores e sistemas de transmissão até sistemas elétricos e eletrônicos. Envolve a compreensão do funcionamento dos veículos e a aplicação de conhecimentos técnicos para manutenção, diagnóstico de problemas e reparos. '
+)
+
+criaCartao(
+    'História',
+    'Qualm foi o motivo da 2° Guerra Mundial?',
+    'foi desencadeada por uma combinação complexa de fatores, com destaque para o imperialismo alemão, o revanchismo após a Primeira Guerra Mundial e a expansão do nazismo e fascismo'
+)
+
+criaCartao(
+    'Musica',
+    'Qual musica do AC/DC pode ser abreviada em D4C?',
+    'A música do AC/DC que pode ser abreviada como D4C é "Dirty Deeds Done Dirt Cheap"'
+)
